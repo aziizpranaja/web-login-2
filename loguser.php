215759ddc745da14b7e767988169f735f404ba7b
aziizpranaja <56227588+aziizpranaja@users.noreply.github.com>
2021-09-30 03:34:48 +0700

    Update loguser.php

--- loguser.php
+++ loguser.php
@@ -136,6 +136,7 @@
                           <?= $row["time_log"]; ?>
                           </td>
                         </tr>
+			 <?php $i++ ?>
                         <?php endforeach; ?>
                       </tbody>
                     </table>
